60abff45225b9e93870020d9d28d73e5a7c7a889
almus <Almas>
2019-07-04 09:40:24 +0600

    base for the app

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart'; 
+
+
+class ProductsOverviewScreen extends StatelessWidget {
+  //const ProductsOverviewScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
