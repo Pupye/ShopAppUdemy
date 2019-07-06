3c15b84f5fc0be2804cb1ff2967b5b1a030a24f8
almus <Almas>
2019-07-06 13:56:26 +0600

    made cart dissmissble with awesome widget

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,28 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final id;
+  final productId; 
   final double price;
   final int quantity;
   final String title;
 
-  const CartItem({Key key, this.id, this.title, this.price, this.quantity})
+  const CartItem({Key key, this.id, this.title, this.price, this.quantity, this.productId})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        child: ListTile(
-          leading: CircleAvatar(
-            child: FittedBox(child:Text('\$ $price'),
-          ),),
-          title: Text(title),
-          subtitle: Text('Total: \$ ${(price * quantity)}'),
-          trailing: Text('$quantity x'),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        padding: EdgeInsets.only(right: 15),
+        margin: EdgeInsets.symmetric(horizontal: 15),
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
         ),
-        padding: EdgeInsets.all(8),
+      alignment: Alignment.centerRight,
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction){
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          child: ListTile(
+            leading: CircleAvatar(
+              child: FittedBox(
+                child: Text('\$ $price'),
+              ),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$ ${(price * quantity)}'),
+            trailing: Text('$quantity x'),
+          ),
+          padding: EdgeInsets.all(8),
+        ),
       ),
     );
   }
