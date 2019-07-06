168065417fe6ac51072d9388ced228b24ba27788
almus <Almas>
2019-07-06 10:13:14 +0600

    new cart provider and working with multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,36 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (ctx)=> Products(), //here we are providing a object that can be listened by its child widgets.
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(
+            value: Products(),
+          ),
+          
+          ChangeNotifierProvider.value(
+            value: Cart(),
+          ),
+        ],
+
+        //here we are providing a object that can be listened by its child widgets.
         child: MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
-      ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
-    ));
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          },
+        ));
   }
 }
---
+++ lib/providers/cart.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CardItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CardItem(
+      {@required this.id,
+      @required this.title,
+      @required this.quantity,
+      @required this.price});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CardItem> _items;
+
+  get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (cardItem) => CardItem(
+                id: cardItem.id,
+                title: cardItem.title,
+                price: cardItem.price,
+                quantity: cardItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CardItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
+  }
+}
